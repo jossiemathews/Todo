17d940a85c5147fd1ae9c97537de0b0f5178a3ee
Manu <jossiemathews@gmail.com>
2025-01-09 20:30:50 +0000

    Add mouse.cpp to demonstrate struct usage and pointer access in C++

---
+++ mouse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+    int x, y;
+
+    void display() {
+        cout << "Point(" << x << ", " << y << ")" << endl;
+    }
+};
+
+int main() {
+    Point p = {10, 20};        // Create a Point object
+    Point* ptr = &p;           // Create a pointer to the object
+
+    // Access members using the arrow operator
+    cout << "x: " << ptr->x << ", y: " << ptr->y << endl;
+
+    // Call the display function
+    ptr->display();
+
+    return 0;
+}

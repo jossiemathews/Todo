98fc36ab8722ac6e697bb17fa26577418dea6744
Manu <jossiemathews@gmail.com>
2025-01-09 16:58:28 +0000

    Refactor main.cpp to enhance vector iteration and add a function demonstrating reference return

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <vector>
+using namespace std;
+
+int& some_function(int i)
+{
+    cout << i << std::endl;
+    int x = 111;
+    static int& v = x;
+    cout<<v<<std::endl;
+    int *var = &v;
+    return v;
+}
 
 int main()
 {
-    using namespace std;
+
     std::vector<int> v = {1, 2, 3, 4, 5};
-    for (int i : v)
+    string s {"Hello World"};
+    for (auto &i : v)
     {
         cout << i << " ";
     }
     cout << std::endl;
+    int r_v = some_function(5);
+    cout << r_v << std::endl;
     return 0;
 }
